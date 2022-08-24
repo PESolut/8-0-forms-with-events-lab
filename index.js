a80a361a846b6e1bbfb5810bf790b0492d89f390
PESolut <jahaadpetty@pursuit.org>
2022-08-24 13:00:21 -0400

    selecting the correct element in our querySelector

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log("Code your solution!")
 // we have to grab our form
-const form = document.querySelector('h1')
+const form = document.querySelector('form')
 console.log(form)
+// listen to the form by 
