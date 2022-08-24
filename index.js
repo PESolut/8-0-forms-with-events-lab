d6a23f0df4e3651aef42c6c3fa3694cc07ce09be
PESolut <jahaadpetty@pursuit.org>
2022-08-24 13:07:18 -0400

    adding psudeo-code

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ console.log("Code your solution!")
 // we have to grab our form
 const form = document.querySelector('form')
 console.log(form)
-// listen to the form by 
+
+// adding input functionality
+// listen to the form
+// on form submission: call a function
+    // store data into a variable
+    // create a new li element
+    // text content is added to our ul, to which is retrieved from our variable that stores input data to end of ul
+    // this dynamically created element is now appeneded to our ul
+
+// adding the crossout functionality
+// populate post input functioanlity completion
+
+
+
